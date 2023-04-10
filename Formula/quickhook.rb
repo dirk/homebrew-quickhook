9de257d8edefdcb640e5de8788bbdafe311799c8
Dirk Gadsden <dirk@esherido.com>
2023-04-09 21:44:55 -0700

    Update `quickhook` to v1.5.0

--- Formula/quickhook.rb
+++ Formula/quickhook.rb
@@ -1,26 +1,18 @@
 class Quickhook < Formula
-  desc "Fast, Unix'y, opinionated Git hook runner"
+  desc "Faster Git hook runner"
   homepage "https://github.com/dirk/quickhook"
-  url "https://github.com/dirk/quickhook/archive/v1.4.0.tar.gz"
-  sha256 "1efa722ad02e868c6e53d771c581ba2978df7c3427291f65600717eed36bd079"
+  url "https://github.com/dirk/quickhook/archive/v1.5.0.tar.gz"
+  sha256 "b32ab57c2bd65b64546bc66b2213b101b13e4063321283901b999dbf820e37a4"
+  license "BSD-3-Clause"
+  head "https://github.com/dirk/quickhook.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    ENV["GO111MODULE"] = "on"
-
-    dir = buildpath/"src/github.com/dirk/quickhook"
-    dir.install buildpath.children
-
-    cd dir do
-      system "go", "mod", "vendor"
-      system "go", "build"
-      bin.install "quickhook"
-    end
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
-    assert_equal "quickhook version #{version}", shell_output("#{bin}/quickhook --version").chomp
+    assert_equal version.to_s, shell_output("#{bin}/quickhook --version").strip
   end
 end
